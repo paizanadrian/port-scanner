d0b49c457c301153e6652ee9a7ada192d438d5d7
paizanadrian <paizanadrian@gmail.com>
2025-01-29 22:38:15 +0200

    First commit

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Titlu aplicație
+st.title("🔍 Aplicație Scanare și Ping")
+
+st.write("""
+    Selectează o pagină din meniul din stânga:
+    - **Scanner de Porturi**: Scanează porturile pentru o adresă IP.
+    - **Ping Domeniu**: Verifică ping-ul unui domeniu/IP.
+""")
